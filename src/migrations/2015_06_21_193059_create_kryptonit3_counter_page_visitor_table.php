9f416b650014afa81ef5ed1119989aada9aa9820
Jesse Szypulski <jesse@firetactic.com>
2015-06-27 11:45:44 -0400

    index foreign keys

--- src/migrations/2015_06_21_193059_create_kryptonit3_counter_page_visitor_table.php
+++ src/migrations/2015_06_21_193059_create_kryptonit3_counter_page_visitor_table.php
@@ -13,10 +13,10 @@ class CreateKryptonit3CounterPageVisitorTable extends Migration
     public function up()
     {
         Schema::create('kryptonit3_counter_page_visitor', function (Blueprint $table) {
-            $table->bigInteger('page_id')->unsigned();
+            $table->bigInteger('page_id')->unsigned()->index();
             $table->foreign('page_id')->references('id')->on('kryptonit3_counter_page')
                 ->onUpdate('cascade')->onDelete('cascade');
-            $table->bigInteger('visitor_id')->unsigned();
+            $table->bigInteger('visitor_id')->unsigned()->index();
             $table->foreign('visitor_id')->references('id')->on('kryptonit3_counter_visitor')
                 ->onUpdate('cascade')->onDelete('cascade');
             $table->timestamp('created_at');
