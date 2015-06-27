9aed58c93d61fa35c99cb8130da62bc11daacae6
Jesse Szypulski <jesse@firetactic.com>
2015-06-26 22:37:08 -0400

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 Route::filter('kryptonit3-cookie',function(){
     if (Cookie::get('kryptonit3-counter') == false) {
-        Cookie::queue('kryptonit3-counter', str_random(80), 2628000);
+        Cookie::queue('kryptonit3-counter', str_random(80), 2628000); // Forever aka 5 years
     }
 });
 
