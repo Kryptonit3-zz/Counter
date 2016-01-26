753775f6381a7fcf340ec72549320783440e0b04
Jesse Szypulski <jesse.szypulski@gmail.com>
2016-01-26 13:33:16 -0500

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,7 @@
 <?php
 
-Route::filter('kryptonit3-cookie',function(){
+Route::group(['middleware' => ['web']], function () {
     if (Cookie::get('kryptonit3-counter') == false) {
         Cookie::queue('kryptonit3-counter', str_random(80), 2628000); // Forever aka 5 years
     }
 });
-
-Route::when('*', 'kryptonit3-cookie');
