ed93a77aa94288fd596f11dc7730b8c8da6a3479
Ivan Kolodiy <35073476+ivankolodii@users.noreply.github.com>
2019-11-12 10:49:15 +0200

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 Route::group(['middleware' => ['web']], function () {
     if (Cookie::get(env('COUNTER_COOKIE', 'kryptonit3-counter')) == false) {
         Cookie::queue(env('COUNTER_COOKIE', 'kryptonit3-counter'), Str::random(80), 2628000); // Forever aka 5 years
