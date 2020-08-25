3a35ef37914bb1b37504ba3c657fe2dcbc7345bc
Hemal Pandya <53688978+hemal-digiflux@users.noreply.github.com>
2020-08-25 11:17:12 +0530

    Update routes.php
    Bug fix for routes file

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,7 @@
+<?php
 Route::group(['middleware' => ['web']], function () {
     if (Cookie::get(env('COUNTER_COOKIE', 'kryptonit3-counter')) == false) {
         Cookie::queue(env('COUNTER_COOKIE', 'kryptonit3-counter'), str_random(80), env('COUNTER_COOKIE_EXPIRATION', 2628000)); // Configurable from ENV
     }
 });
+?>
